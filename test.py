0c53a0dfb12d89a3ce0dc2a716c48717205cf1db
Jan Martin Reckel <jm.reckel@t-online.de>
2024-09-11 17:37:02 +0200

    Update tests

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ TESTFILE = 'schedule_test.csv'
 def test_get_todays_bible_reading():
     # Create a sample schedule.csv file
     with open(TESTFILE, 'w') as file:
-        file.write("2022-01-01,Matthew 1,Genesis 1\n")
-        file.write("2022-01-02,Matthew 2,Genesis 2\n")
-        file.write('2022-01-03,Matthew 3,"Genesis 3"\n')
-        file.write(f"{datetime.date.today().strftime('%Y-%m-%d')},Matthew 4,Genesis 4\n")
+        file.write("01-01-22,Matthew 1,Genesis 1\n")
+        file.write("01-02-22,Matthew 2,Genesis 2\n")
+        file.write('01-03-22,Matthew 3,"Genesis 3"\n')
+        file.write(f"{datetime.date.today().strftime('%m-%d-%y')},Matthew 4,Genesis 4\n")
         file.close()
 
     # Test case: today's date is in the schedule
